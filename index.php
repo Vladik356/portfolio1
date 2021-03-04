72cad5f208a1705f8290110e7e2f916c5dde75ae
Vladislav <44534111+Vladik356@users.noreply.github.com>
2021-03-04 12:41:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Личный сайт Владислава</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="content">
+
+	<div class="header">
+		<a href="index.php">Главная</a>
+		<a href="puzzle.html">Загадки</a>
+		<a href="guess.html">Угадайка</a>
+		<a href="guess-2.html">Угадайка мультиплеер</a>
+		<a href="passgen.html">Генератор паролей</a>
+	</div>
+
+	<h1>Личный сайт Владислава</h1>
+
+	<div class="center">
+	<img src="img/photo1.jpg" alt="фотография">
+		<div class="box_text">
+			<p>Добрый день. Меня зовут <b>Владислав.</b>
+			<br>Я начинающий программист. И это мой первый сайт.<br>Ниже есть информация обо мне и возможность поиграть в, сделанные мной игры :)</p>
+
+			<p>Вот мои личный <i><a href="https://vk.com/ilchenkovladislav">данные.</a></i></p>
+
+			<p>Здесь вы можете поиграть в игры:
+			<a href="puzzle.html">Загадки,</a>
+			<a href="guess.html">Угадайка,</a>
+			<a href="guess-2.html">Угадайка мультиплеер,</a>
+			<a href="passgen.html">Генератор паролей.</a>
+			</p>
+		</div>
+	</div>
+</div>
+<div class="footer">
+	Copyright &copy; <?php echo date("Y");?> Vladislav
+</div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<div class="header">
+		<a href="index.html">Главная</a>
+		<a href="puzzle.html">Загадки</a>
+		<a href="guess.html">Угадайка</a>
+		<a href="guess-2.html">Угадайка мультиплеер</a>
+		<a href="passw-generator.html">Генератор паролей</a>
+	</div>
